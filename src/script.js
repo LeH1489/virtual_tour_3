a2163b575457608a01b95f3f775e558b838b616a
LeH1489 <93253543+LeH1489@users.noreply.github.com>
2024-04-15 11:06:56 +0700

    add thu vien

---
+++ src/script.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import * as lilGui from "lil-gui";
+import gsap from "gsap";
